fd4bb6e8c254b44db1a66815f01e04cb6ea7d625
Роман Семашкевич <semaroman@mail.ru>
2023-01-19 21:14:27 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+import.java.util.function.Supplier;
+import.java.util.function.BinaryOperator;
+import.java.util.function.UnaryOperator;
+import.java.util.function.Predicate;
+import.java.util.function.Consumer;
+
+class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
+    }
+}
+
+class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Unteger> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+}
